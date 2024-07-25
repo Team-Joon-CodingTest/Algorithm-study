c38672e0884b25a12e0af8bac4d30a477b712643
egg-silver <v6984@naver.com>
2024-07-26 01:44:23 +0900

    라이브 코테 1번 수정

--- test5/문제1/이지은.js
+++ test5/문제1/이지은.js
@@ -1,11 +1,11 @@
 function solution(food) {
-  var answer = [];
+  var answer = '';
 
   for (let i = 1; i < food.length; i++) {
     let num = Math.floor(food[i] / 2);
 
-    answer.push(food[i] * num);
+    answer += i.toString().repeat(num);
   }
 
-  return answer;
+  return answer + '0' + answer.split('').reverse().join('');
 }
