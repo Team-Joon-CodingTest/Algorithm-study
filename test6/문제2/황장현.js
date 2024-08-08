f211547dc9f71c884401cd25e2384b08dcf009f3
hwang <hgh1057@gmail.com>
2024-08-08 23:55:55 +0900

    6차 2번 문제풀이

--- test6/문제2/황장현.js
+++ test6/문제2/황장현.js
@@ -12,13 +12,30 @@ readline
   .on('close', function () {
     // 솔루션 작성
     const A = parseInt(input[0]);
-    const primeNum = [2, 3, 5, 7];
+    let inputNum = A;
+    if (inputNum <= 1) return 2;
+    if (inputNum <= 3) return A;
 
-    for (let i = 0; i < primeNum.length; i++) {
-      if (A / primeNum[i] === 1) {
-        console.log(A);
-        process.exit();
+    while (1) {
+      if (inputNum % 2 === 0 || inputNum % 3 === 0) {
+        inputNum += 1;
+        continue;
       }
+
+      let isPrime = true;
+      for (let i = 5; i * i <= inputNum; i += 6) {
+        if (inputNum % i === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+
+      if (isPrime) {
+        console.log(inputNum);
+        return inputNum;
+      }
+
+      inputNum += 1;
     }
 
     process.exit();
@@ -29,7 +46,6 @@ readline
 정수 n(0 ≤ n ≤ 4*10^9)가 주어졌을 때, n보다 크거나 같은 소수 중 가장 작은 소수 찾는 프로그램을 작성하시오.
 
 1. 인풋값이 소수인지 체크
-1-1. 소수 체크 2,3,5,7로 나눴을 때 소수점이 나오면 소수 
 2. 아니면 1씩 증가
 3. 다시 체크 
 4. 반복
