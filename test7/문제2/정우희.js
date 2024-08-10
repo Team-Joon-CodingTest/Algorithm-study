4bba19034a37841f76524a7f089ff269da73251a
woohee <aldkgha@naver.com>
2024-08-10 13:15:38 +0900

    7주차 문제2 다시풀기

--- test7/문제2/정우희.js
+++ test7/문제2/정우희.js
@@ -6,4 +6,20 @@ const input = fs
   .split('\n')
   .map(Number);
 
-function solution(num) {}
+function solution(num) {
+  if (num === 1) return false;
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === 0) break;
+
+  let count = 0;
+  for (let j = input[i] + 1; j <= input[i] * 2; j++) {
+    if (solution(j)) count++;
+  }
+  console.log(count);
+}
