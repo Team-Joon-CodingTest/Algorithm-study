e134a1718711ec2ca6c3a063950cd8d5d66bdbc6
gmlrude <qkrgmlrud00@gmail.com>
2024-08-10 13:06:41 +0900

    7차 2번 재풀이

--- test7/문제2/박희경.py
+++ test7/문제2/박희경.py
@@ -1,20 +1,19 @@
 """
 n보다 크고 2n 보다 작거나 같은 소수 개수
 시간 초과..
-이미 구해져있는건 바로 쓰는 DP 문제인가?
+에르토스테네스의 체 : 소수를 대량으로 빠르게 찾는 알고리즘
 """
 import sys
 
 input = sys.stdin.readline
+num = 123456 * 2 + 1  # 길이는 n의 최대 범위로 설정
+num_list = [1] * num  # 소수가 아니라면 0, 맞으면 1
 
-
-# 소수 개수 구하는 함수
-def find_prime(num):
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:  # 소수가 아님
-            return False
-    return True
-
+for i in range(1, num):
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            num_list[i] = 0
+            break
 
 while True:
     n = int(input())
@@ -23,7 +22,5 @@ while True:
     if n == 0:
         break
     for i in range(n + 1, 2 * n + 1):
-        if find_prime(i):
-            count += 1
-
+        count += num_list[i]
     print(count)
