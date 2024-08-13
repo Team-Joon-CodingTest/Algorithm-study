ab4472a568a25383f2543a4acdc9d930418c00fd
kingjinyong <wlsdydwhswkf@gmail.com>
2024-08-13 16:01:52 +0900

    8차 2번 문제풀이

--- test8/문제1/김진용.py
+++ test8/문제1/김진용.py
@@ -5,7 +5,7 @@ def is_prime(a):
     if a < 2:
         return False
     for i in range(2, int(a ** 0.5) + 1):
-        if a % i  == 0:  # 나누어진다면 바로 False 반환
+        if a % i == 0:  # 나누어진다면 바로 False 반환
             return False
     return True  # 나누어지는 수가 하나도 없다면 True 반환
 
