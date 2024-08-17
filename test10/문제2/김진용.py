2e762bbd6e838586be4a95d11878d6b8ebca11e5
kingjinyong <wlsdydwhswkf@gmail.com>
2024-08-17 13:24:49 +0900

    10차 2번 문제풀이(주석추가)

--- test10/문제2/김진용.py
+++ test10/문제2/김진용.py
@@ -2,12 +2,13 @@ s = list(input())
 
 dic = {'a': 1, 'b': 0}
 
-c = 'a'
+c = 'a' # 스위치
 for i in range(1, len(s)):
-    if s[i] != s[i-1]:
+    if s[i] != s[i-1]: # 이전과 다를 경우 바꿔주기
         if c == 'a':
             c = 'b'
         else:
             c = 'a'
         dic[c] += 1
+
 print(min(dic.values()))
