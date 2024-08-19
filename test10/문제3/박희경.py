cd89d643b03940381a267473dad7fdf2af5d769a
gmlrude <qkrgmlrud00@gmail.com>
2024-08-19 17:01:19 +0900

    10차 3번 재풀이

--- test10/문제3/박희경.py
+++ test10/문제3/박희경.py
@@ -1,24 +1,39 @@
+"""
+1. 3번 등장
+2. 다음에 한 번 더 문자 삽입
+"""
 import sys
 
 input = sys.stdin.readline
 
 t = int(input())
 
-dict = {}
+
 for _ in range(t):
+    dic = {}  # 문자 숫자 세기
     message = input().strip()
     message = list(message)
     key = sorted(set(message))
 
     count = 0
+    status = "OK"
     for k in key:
-        dict[k] = 0
+        dic[k] = 0
 
-    for m in message:
-        dict[m] += 1
+    i = 0
+    while i < len(message):
+        char = message[i]   # 현재 문자
+        dic[char] += 1      # 해당 문자(char)의 개수 카운팅
 
-    for value in dict.values():  # 다시 확인할 부분
-        if value % 3 == 0:
-            print("FAKE")
-        else:
-            print("OK")
+        if dic[char] == 3:    # 3번 등장
+            # 다음에 나오는 문자와 현재 문자가 같다면 진짜 메시지
+            if i+1 < len(message) and message[i+1] == char:
+                status = "OK"
+                dic[char] = 0   # 초기화
+                i += 1
+            else:
+                status = "FAKE"
+                break
+        i += 1
+
+    print(status)
