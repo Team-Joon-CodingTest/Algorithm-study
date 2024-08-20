30884614eec76d448a40402f66fa0d7859fac466
kingjinyong <wlsdydwhswkf@gmail.com>
2024-08-20 22:15:18 +0900

    10차 1번 문제풀이

--- test10/문제1/김진용.py
+++ test10/문제1/김진용.py
@@ -1,12 +1,26 @@
-from itertools import permutations
+from collections import deque
 
 s, p = map(int, input().split())
 dna = list(input())
-dnac = {'A': dna.count('A'), 'C': dna.count('C'), 'G': dna.count('G'), 'T': dna.count('T')}
-mini = {'A': 0, 'C': 0, 'G': 0, 'T': 0, }
-mini['A'], mini['C'], mini['G'], mini['T'] = map(int, input().split())
+A, C, G, T = map(int, input().split())
 
-for i in ('A', 'C', 'G', 'T'):
-    if dnac[i] < mini[i]: # 최소 개수보다 적다면 만들 수 있는 비밀번호가 없음
-        print(0)
-        exit()
+left, right = 0, p-1 # 포인터 설정
+ary = []
+ary = deque(dna[left:right])
+dic = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+for i in ary:
+    dic[i] += 1
+
+cnt = 0
+
+while right < s:
+    dic[dna[right]] += 1 # 구간 설정
+
+    if dic['A'] >= A and dic['C'] >= C and dic['G'] >= G and dic['T'] >= T:
+        cnt += 1
+
+    dic[dna[left]] -= 1
+    left += 1
+    right += 1
+
+print(cnt)
