3677d33f01321629e916d8a57b747fa3baeb5d52
kingjinyong <wlsdydwhswkf@gmail.com>
2024-08-22 22:42:53 +0900

    12차 3번 문제풀이(검색해서 공부했음)

---
+++ test12/문제3/김진용.py
@@ -0,0 +1,13 @@
+import heapq    # 기본적으로 최소힙임
+
+n = int(input())
+heap = []
+for i in range(n):
+    x = int(input())
+    if x > 0:
+        heapq.heappush(heap, -x)    # 음수 붙여서 넣어주면 최대 힙으로 변환 가능
+    elif x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(-(heapq.heappop(heap)))   # 제일 값이 작은 음수에 다시 음수 붙여버리면 됨
