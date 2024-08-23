bc1c4af26d731b95002b71d90dbd9a6f350d32b1
gmlrude <qkrgmlrud00@gmail.com>
2024-08-23 18:43:36 +0900

    13차 1번 재풀이

--- test13/문제1/박희경.py
+++ test13/문제1/박희경.py
@@ -9,21 +9,24 @@ input = sys.stdin.readline
 
 t = int(input())
 
-# 중요도가 높은 문서의 인덱스 구하기
-# def find_idx(priority, queue):
-#     while queue:
-#         document = queue.popleft()
-#         if document < property:
-#             queue.append(document)
-#         elif document == property:
-#             return
-
 for _ in range(t):
     n, m = map(int, input().split())
-    queue_list = list(map(int, input().split()))
-    queue = deque(queue_list)
+    queue = deque(map(int, input().split()))
+    cnt = 0
 
-    queue_list[m]
-    # priority = max(queue)
+    while queue:
+        priority = max(queue)
+        first = queue.popleft()  # 큐 맨 앞에 있는 값 빼기
+        m -= 1  # 뺏으니까 한 칸 당기기
 
+        if first == priority:
+            cnt += 1  # 문서 출력 됐으니 카운팅
+            if m < 0:
+                print(cnt)
+                break
 
+        # 우선순위가 높은 문서가 아닐 때
+        else:
+            queue.append(first)  # 맨 뒤에 저장
+            if m < 0:
+                m = len(queue) - 1
