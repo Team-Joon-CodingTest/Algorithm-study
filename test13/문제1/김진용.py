872a66f6304ae48245a2d0219b82749416f59712
kingjinyong <wlsdydwhswkf@gmail.com>
2024-08-23 14:52:19 +0900

    13차 2번 문제풀이

---
+++ test13/문제1/김진용.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+ary = list(map(int, input().split()))
+ary2 = []
+
+for i in range(len(ary)):
+    ary2.append([i, ary[i]])
+
+while ary2:
+
+
+
+
+# [[0, 1], [1, 1], [2, 9], [3, 1], [4, 1], [5, 1]]
