26c5556f61fe4cc917f9f3b806e06c6117ad6a57
hwang <hgh1057@gmail.com>
2024-08-26 19:49:06 +0900

    13차 1번 문제풀이

--- test13/문제1/황장현.js
+++ test13/문제1/황장현.js
@@ -7,19 +7,33 @@ const input = require('fs')
 
 function solution(input) {
   const testCaseCount = input.shift()[0];
-
   const results = [];
 
   for (let i = 0; i < testCaseCount; i++) {
     const [N, M] = input.shift();
-
-    console.log(N, M);
-
     const priorities = input.shift();
-    console.log('priorities: ', priorities);
+    let outIndex = 0;
+
+    const queue = priorities.map((priority, index) => ({ index, priority }));
+
+    while (queue.length > 0) {
+      let currentFile = queue.shift();
+
+      let maxPriority = Math.max(...queue.map((file) => file.priority));
+
+      if (currentFile.priority < maxPriority) {
+        queue.push(currentFile);
+      } else {
+        outIndex++;
+        if (currentFile.index === M) {
+          results.push(outIndex);
+          break;
+        }
+      }
+    }
   }
 
-  return results;
+  return results.join('\n');
 }
 
 console.log(solution(input));
