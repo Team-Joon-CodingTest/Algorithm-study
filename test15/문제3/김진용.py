7722440bab02d87f34db431344266a72dbdb7f4c
kingjinyong <wlsdydwhswkf@gmail.com>
2024-08-29 22:23:17 +0900

    15차 3번 문제풀이

--- test15/문제3/김진용.py
+++ test15/문제3/김진용.py
@@ -9,14 +9,21 @@ for i in range(1, qs + 1):
 
 m = 0
 result = 0
-lr = 0
-rr = 0
+cnt = 0
 while sary:
-    if q[0] not in sary:
-        q.popleft()
-    elif q[0] == sary[0]:
+    if q[0] == sary[0]:
         q.popleft()
         sary.pop(0)
     else:
-        c1 = q.index(sary[-1]) - q.index(sary[0])
-        c2 = q.index(sary[0]) - q.index(q[0])
+        c1 = q.index(q[-1]) - q.index(sary[0])   # 마지막 인덱스 - 찾으려는 수의 인덱스
+        c2 = q.index(sary[0])                    # 찾으려는 수의 인덱스 - 큐의 첫번째 인덱스(0)
+
+        if c1 < c2:   # 찾고자 하는 값이 큐의 마지막 인덱스와 더 가깝다면
+            while q[0] != sary[0]:
+                q.rotate(1)
+                cnt += 1
+        else:
+            while q[0] != sary[0]:
+                q.rotate(-1)
+                cnt += 1
+print(cnt)
