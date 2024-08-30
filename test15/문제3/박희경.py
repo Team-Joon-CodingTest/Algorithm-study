8eee95e20681691753dacf2eb6ea0f69d3c849c3
gmlrude <qkrgmlrud00@gmail.com>
2024-08-30 13:58:41 +0900

    15차 3번 재풀이

--- test15/문제3/박희경.py
+++ test15/문제3/박희경.py
@@ -2,7 +2,7 @@
 1. popleft()
 2. append(popleft())
 3. appendleft(pop())
-틀림 (시간부족)
+- 접근은 맞았음
 """
 import sys
 from collections import deque
@@ -15,19 +15,20 @@ q = deque([i for i in range(1, n+1)])
 index = list(map(int, input().split()))
 cnt = 0
 
-for i in range(m):
-    idx = index[i]
-    if q[0] == idx:
-        print(q[0])
-        q.popleft()
-    else:
-        if idx <= len(q)//2:
-            print("2번 연선")
-            q.append(q.popleft())
-            cnt += 1
+for idx in index:
+    while True:     # 놓친 부분
+        if q[0] == idx:
+            q.popleft()
+            break
         else:
-            print("3번 연선")
-            q.appendleft(q.pop())
-            cnt += 1
+            if q.index(idx) < len(q)/2:    # 큐를 반으로 나눴을 때보다 뽑으려는 인덱스가 작을 때
+                while q[0] != idx:  # 놓친 부분. 일치할 때까지 연산 반복
+                    # 2번 연산
+                    q.append(q.popleft())
+                    cnt += 1
+            else:
+                while q[0] != idx:
+                    # 3번 연산
+                    q.appendleft(q.pop())
+                    cnt += 1
 print(cnt)
-
