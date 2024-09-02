a59ac341d4fa63675b3d9df492d9cf9a6d7683cb
hwang <hgh1057@gmail.com>
2024-09-02 21:12:16 +0900

    17회차 2번 문제 풀이(풀이중)

---
+++ test17/문제2/황장현.js
@@ -0,0 +1,12 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.split(' ').map(Number));
+
+function solution(input) {
+  const [N, M] = input.shift();
+}
+
+console.log(solution(input));
