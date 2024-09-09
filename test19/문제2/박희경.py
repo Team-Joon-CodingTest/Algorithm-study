e8315f29fbb3a7ab57463b9473f94cbc016a5ddd
gmlrude <qkrgmlrud00@gmail.com>
2024-09-09 11:22:33 +0900

    19차 2번 재풀이

--- test19/문제2/박희경.py
+++ test19/문제2/박희경.py
@@ -1,19 +1,18 @@
 import sys
 
-
 input = sys.stdin.readline
 
-n = list(map(int, input().rstrip()))
+n = list(map(str, input().rstrip()))
+n.sort(reverse=True)
 
 sum = 0
 
-if 0 not in n:  # 10의 배수가 될 수 없으므로
+if '0' not in n:  # 10의 배수가 될 수 없으므로
     print(-1)
 else:
     for i in n:
-        sum += i
-    if sum % 3 != 0:  # 3의 배수
+        sum += int(i)  # 각 자리수의 합이 3의 배수인지 판별
+    if sum % 3 != 0:  # 3의 배수 아니라면
         print(-1)
     else:
         print(''.join(n))
-
