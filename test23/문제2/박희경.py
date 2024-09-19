7fde3fdc27e867ee2ed454cfebe2c992bc1c7c89
gmlrude <qkrgmlrud00@gmail.com>
2024-09-19 21:43:23 +0900

    23차 2번 다시 풀이

--- test23/문제2/박희경.py
+++ test23/문제2/박희경.py
@@ -1,20 +1,22 @@
 import sys
-from itertools import *
+from collections import defaultdict
 
 input = sys.stdin.readline
 
 t = int(input()) 
-dict = {}
 
 
 for _ in range(t):
+    dict = defaultdict(list)  # 기본값이 리스트인 딕셔너리
     n = int(input())    # 의상 수
     for _ in range(n):
         name, type = map(str, input().split())
         
-        dict[type] = name
+        dict[type].append(name)
 
     cnt = 1
 
-    for t, n in dict.items:
-        cnt += len(dict[t])+1
+    for key in dict.keys():
+        cnt *= len(dict[key]) + 1   # 해당 의상 종류의 의상을 입지 않는 경우를 위해 1 더한다.
+    
+    print(cnt - 1)  # 아무것도 안 입는 경우를 제외한다.
