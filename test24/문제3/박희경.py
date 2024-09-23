c3e037b228791cc57219ef5412cfd4f2435b619e
gmlrude <qkrgmlrud00@gmail.com>
2024-09-23 18:06:30 +0900

    24차 3번 다시 풀이

--- test24/문제3/박희경.py
+++ test24/문제3/박희경.py
@@ -3,36 +3,55 @@ import sys
 input = sys.stdin.readline
 
 t = int(input())
-cnt = 0
 
 
-def is_graph(start, mid, dest):
+def dfs(x):
     global cnt
-    visited[mid] = 1    # 방문처리
-    if start == dest:   # 순열 사이클의 경우
-        cnt += 1
-        print("엥", start, dest)
-        print(visited)
-        for i in range(1, n+1):
-            if visited[i] != 1:
-                start = i
-                is_graph(start, start, arr2[start - 1][1])
-                break
-            # 모두 방문했을 때 처리
-    print(start, mid, dest)
-    is_graph(start, dest, arr2[dest-1][1])
-
+    visited[x] = 1  # 방문처리
+    nx = arr[x]
+    if not visited[nx]:
+        dfs(nx)
 
 
 for _ in range(t):
     n = int(input())
-    arr = list(map(int, input().split()))
+    arr = [0] + list(map(int, input().split()))
+    visited = [0] * (n + 1)
+    cnt = 0
 
-    arr2 = list(zip(range(1, n+1), arr))
-    visited = [0] * (n+1)
-    # [(1, 3), (2, 2), (3, 7), (4, 8), (5, 1), (6, 4), (7, 5), (8, 6)]
-    is_graph(arr2[0][0], arr2[0][0], arr2[0][1])
-
-    # print(arr2[0][0], arr2[0][1])
+    for i in range(1, n + 1):
+        if not visited[i]:  # 방문하지 않은 노드부터
+            dfs(i)
+            cnt += 1  # 사이클이 있을 때
 
     print(cnt)
+
+# 이전 코드 : 시간 초과
+# def is_graph(start, mid, dest):
+#     global cnt
+#     visited[mid] = 1  # 방문처리
+#     if start == dest:  # 순열 사이클의 경우
+#         cnt += 1
+#         for i in range(1, n + 1):
+#             if visited[i] != 1:
+#                 start = i
+#                 is_graph(start, start, arr2[start - 1][1])
+#                 # print(visited)
+#                 break
+#     # 모두 방문했을 때 처리
+#     if '0' not in str(visited):
+#         return cnt
+#     is_graph(start, dest, arr2[dest - 1][1])
+#
+#
+# for _ in range(t):
+#     cnt = 0
+#     n = int(input())
+#     arr = list(map(int, input().split()))
+#
+#     arr2 = list(zip(range(1, n + 1), arr))
+#     visited = [1] + [0] * n
+#     # [(1, 3), (2, 2), (3, 7), (4, 8), (5, 1), (6, 4), (7, 5), (8, 6)]
+#     is_graph(arr2[0][0], arr2[0][0], arr2[0][1])
+#
+#     print(cnt)
