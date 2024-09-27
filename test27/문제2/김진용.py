3a1e6f763411e173ca88ac648809f10284226368
kingjinyong <wlsdydwhswkf@gmail.com>
2024-09-27 15:41:21 +0900

    27차 2번 문제풀이

---
+++ test27/문제2/김진용.py
@@ -0,0 +1,13 @@
+from itertools import *
+import sys
+input = sys.stdin.readline
+N, S = map(int, input().split())
+
+ary = list(map(int, input().split()))
+
+count = 0
+for i in range(1, N+1):
+    for j in combinations(ary, i):
+        if sum(j) == S:
+            count += 1
+print(count)
