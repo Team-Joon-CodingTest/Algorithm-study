0b1833a66586728491c55dc25b252464e6a21c22
hwang <hgh1057@gmail.com>
2024-10-04 14:48:49 +0900

    29회차 1번 문제 풀이

--- test29/문제1/황장현.js
+++ test29/문제1/황장현.js
@@ -8,37 +8,37 @@ const input = require('fs')
 function solution(input) {
   const [N, K] = input[0];
   const maxPosition = 100000;
-  const visited = Array(maxPosition + 1).fill(false);
-  const queue = [];
 
-  queue.push([N, 0]);
-
-  visited[N] = true;
-
-  function bfs(N, time) {
-    while (queue.length > 0) {
-      const [currentPosition, time] = queue.shift();
-
-      if (currentPosition === K) return time;
-
-      if (currentPosition - 1 >= 0 && !visited[currentPosition - 1]) {
-        visited[currentPosition - 1] = true;
-        queue.push([currentPosition - 1, time + 1]);
-      }
-
-      if (currentPosition + 1 <= maxPosition && !visited[currentPosition + 1]) {
-        visited[currentPosition + 1] = true;
-        queue.push([currentPosition + 1, time + 1]);
-      }
-
-      if (currentPosition * 2 <= maxPosition && !visited[currentPosition * 2]) {
-        visited[currentPosition * 2] = true;
-        queue.push([currentPosition * 2, time]);
-      }
-    }
+  if (N >= K) {
+    return N - K;
   }
 
-  return bfs(N, 0);
+  const visited = Array(maxPosition + 1).fill(false);
+  const queue = [[N, 0]];
+  visited[N] = true;
+
+  while (queue.length > 0) {
+    const [currentPosition, time] = queue.shift();
+
+    if (currentPosition === K) {
+      return time;
+    }
+
+    if (currentPosition * 2 <= maxPosition && !visited[currentPosition * 2]) {
+      visited[currentPosition * 2] = true;
+      queue.unshift([currentPosition * 2, time]);
+    }
+
+    if (currentPosition - 1 >= 0 && !visited[currentPosition - 1]) {
+      visited[currentPosition - 1] = true;
+      queue.push([currentPosition - 1, time + 1]);
+    }
+
+    if (currentPosition + 1 <= maxPosition && !visited[currentPosition + 1]) {
+      visited[currentPosition + 1] = true;
+      queue.push([currentPosition + 1, time + 1]);
+    }
+  }
 }
 
 console.log(solution(input));
