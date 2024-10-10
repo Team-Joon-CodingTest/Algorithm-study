3fc21571d06debdd566c2afcc404e2dc87fbaa54
hwang <hgh1057@gmail.com>
2024-10-10 19:24:02 +0900

    31회차 1번 문제풀이

--- test31/문제1/황장현.js
+++ test31/문제1/황장현.js
@@ -1,15 +1,47 @@
-const input = require('fs')
+const fs = require('fs');
+const input = fs
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
   .toString()
   .trim()
   .split('\n')
-  .map((el) => el.split(' ').map(Number));
+  .map(Number);
+
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
 
 function solution(input) {
-  const T = input[0][0];
-  const testCase = input(1);
-  const primeNumbers = [2, 3];
-  const maxPrimeNumber = 100000;
+  const T = input[0];
+  const testCases = input.slice(1);
+  const results = [];
+
+  for (let i = 0; i < T; i++) {
+    const k = testCases[i];
+
+    if (isPrime(k)) {
+      results.push(0);
+      continue;
+    }
+
+    let prevPrime = k - 1;
+    let nextPrime = k + 1;
+
+    while (prevPrime > 1 && !isPrime(prevPrime)) {
+      prevPrime--;
+    }
+
+    while (!isPrime(nextPrime)) {
+      nextPrime++;
+    }
+
+    results.push(nextPrime - prevPrime);
+  }
+
+  return results.join('\n');
 }
 
 console.log(solution(input));
