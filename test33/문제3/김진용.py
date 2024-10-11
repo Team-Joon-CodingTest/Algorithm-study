87d9e8ca8e2176e84518787ab7378eed92ee4587
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-11 16:33:10 +0900

    33차 3번 문제풀이

--- test33/문제3/김진용.py
+++ test33/문제3/김진용.py
@@ -1,19 +1,33 @@
+import sys
+input = sys.stdin.readline
 t = int(input())
+
+def binary_search(target):
+    start = 0
+    end = len(ary) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if ary[mid] == target:
+            return True
+        elif ary[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return False
+
+
 for _t in range(t):
     n = int(input())
     ary = list(map(int, input().split()))
+
+    ary.sort()  # 정렬
+
     cnt = 0
-    dic = {}
-    for i in ary:
-        dic[str(i)] = 1
-    print(dic)
 
     for i in range(len(ary) - 1):
-        for j in range(i + 2, len(ary)):
-            first = ary[i]
-            second = ary[j]
-            mid = (ary[i] + ary[j])//2
-            print(mid)
-            if dic[str(mid)]:
+        for j in range(i + 1, len(ary)):
+            d = abs(ary[j] - ary[i])    # 첫 번째 점, 두 번째 점 간 거리 구하기
+            if binary_search(ary[j] + d):
                 cnt += 1
     print(cnt)
