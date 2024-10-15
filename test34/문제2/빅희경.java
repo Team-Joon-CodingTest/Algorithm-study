9e7714323b70dca322b666c29909eb367ba253fc
gmlrude <qkrgmlrud00@gmail.com>
2024-10-15 16:44:04 +0900

    34차 2번 다시 풀이

--- test34/문제2/빅희경.java
+++ test34/문제2/빅희경.java
@@ -1,24 +1,29 @@
-// 투포인터가 아닌 스택 문제라고 함..
+/*
+스택 문제
+스택에 인덱스를 넣는다.
+비교해서 값을 넣고 마지막 값과 비교 작으면 넣지 않음
+*/
 import java.util.*;
 
 class Solution {
-    public List solution(int[] numbers) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(int[] numbers) {
+        int [] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<>();
 
-        int start = 0;
-        int end = 0;
-        int maxNum = 0;
-
-        for (end = start; end < numbers.length; end++) {
-
-            if (numbers[start] < numbers[end]) {
-                answer.add(numbers[end]);
-                start++;
+        stack.push(0);
+        for (int i = 1; i < numbers.length; i++) {
+            // 현재 값이 최상단 값(stack.peek())보다 크면 뒤에서 큰 수
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                answer[stack.pop()] = numbers[i];
             }
-//            else {
-//                answer.add(-1);
-            }
+            stack.push(i);
         }
+
+        // 스택에 남은 인덱스들은 -1로
+        while(!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
         return answer;
     }
 }
