8483a19a20056212b3131ee40128bc1c2ebc3b35
kingjinyong <wlsdydwhswkf@gmail.com>
2024-10-17 21:12:49 +0900

    35차 2번 문제풀이

---
+++ test35/문제2/김진용.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+ary = list(map(int, input().split()))
+
+start = 0
+end = 1
+cnt = 0
+while end <= n and start <= end:
+    total = sum(ary[start:end])
+
+    if total == m:
+        cnt += 1
+        end += 1
+    elif total > m:
+        start += 1
+    else:
+        end += 1
+print(cnt)
+
