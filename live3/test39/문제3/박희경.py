7d7d910af60cc567255bb0cbd6b1c9e0ab8f0c49
Heekyeong Park <101381901+gmlrude@users.noreply.github.com>
2024-10-28 03:38:50 +0900

    오타 수정

--- live3/test39/문제3/박희경.py
+++ live3/test39/문제3/박희경.py
@@ -13,7 +13,7 @@ def bt(depth, start):
     if depth == n:
         sum_list.add(sum(stack))
         return
-    for i in range(start, len(rome)):    # 1, 5 / 5, 1 이렇게 중복 계산을 안 하도록 start 지점 해야 함
+    for i in range(start, len(rome)):    # 1, 5 / 5, 1 이렇게 중복 계산을 안 하도록 start 지정 해야 함
         stack.append(rome[i])
         bt(depth + 1, i)
         stack.pop()
