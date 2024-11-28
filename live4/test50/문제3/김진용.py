8ac8d09f7ca83b32573dc2263f957ebb7e2cfbd0
kingjinyong <wlsdydwhswkf@gmail.com>
2024-11-29 07:57:10 +0900

    50차 3번 문제풀이

---
+++ live4/test50/문제3/김진용.py
@@ -0,0 +1,11 @@
+def solution(s, skip, index):
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    answer = ""
+    for i in list(skip):
+        alpha = alpha.replace(i, "")
+
+    for a in s:
+        answer += alpha[(alpha.find(a) + index) % len(alpha)]
+    return answer
+
+print(solution("aukks", "wbqd", 5))
