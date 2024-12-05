7e433d7390dca5ab9aaf824aa776838a6c9f0a23
egg-silver <v6984@naver.com>
2024-12-05 21:33:18 +0900

    53차 라이브코테 3번 성공>v<

--- live5/test53/문제3/이지은.js
+++ live5/test53/문제3/이지은.js
@@ -1,5 +1,5 @@
 function solution(X, Y) {
-  let answer = [];
+  let answer = '';
 
   let Xmap = new Map();
   for (let i = 0; i < X.length; i++) {
@@ -8,22 +8,25 @@ function solution(X, Y) {
 
   let Ymap = new Map();
   for (let j = 0; j < Y.length; j++) {
-    Ymap.set(Y[j], (Ymap.has(X[j]) ? Ymap.get(X[j]) : 0) + 1);
+    Ymap.set(Y[j], (Ymap.has(Y[j]) ? Ymap.get(Y[j]) : 0) + 1);
   }
-  console.log(Xmap, Ymap);
 
   for ([xKey, xCount] of Xmap) {
     for ([yKey, yCount] of Ymap) {
       if (xKey === yKey) {
-        answer.push(xKey.repeat(Math.min(xCount, yCount)));
+        answer += xKey.repeat(Math.min(xCount, yCount));
       }
     }
   }
   if (answer.length === 0) return '-1';
   if (answer[0] === '0') return '0';
-  return answer
+  
+  answer = answer
+    .split('')
     .map(Number)
     .sort((a, b) => b - a)
     .join('');
+
+  return answer;
 }
-console.log(solution('5525', '1255'));
+console.log(solution('9111', '9111'));
