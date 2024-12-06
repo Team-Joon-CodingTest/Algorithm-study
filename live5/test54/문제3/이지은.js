6dd41652b53e5f1b105746741296b14c412090fa
egg-silver <v6984@naver.com>
2024-12-06 18:45:11 +0900

    54차 라이브코테 3번 풀었어용

--- live5/test54/문제3/이지은.js
+++ live5/test54/문제3/이지은.js
@@ -1,18 +1,23 @@
 function solution(n, lost, reserve) {
-  let answer = n - lost.length;
-  let arr = Array(n);
+  let realLost = lost
+    .filter((el) => !reserve.includes(el))
+    .sort((a, b) => a - b);
 
-  for (let i = 0; i < lost.length; i++) {
-    console.log(reserve);
-    
-    for (let j = 0; j < reserve.length; j++) {
-      if (lost[i] - 1 === reserve[j]) {
+  let realReserve = reserve
+    .filter((el) => !lost.includes(el))
+    .sort((a, b) => a - b);
+
+  let answer = n - realLost.length;
+
+  for (let i = 0; i < realLost.length; i++) {
+    for (let j = 0; j < realReserve.length; j++) {
+      if (realLost[i] - 1 === realReserve[j]) {
         answer++;
-        reserve.splice(j, 1);
+        realReserve.splice(j, 1);
         break;
-      } else if (lost[i] + 1 === reserve[j]) {
+      } else if (realLost[i] + 1 === realReserve[j]) {
         answer++;
-        reserve.splice(j, 1);
+        realReserve.splice(j, 1);
         break;
       }
     }
@@ -22,4 +27,4 @@ function solution(n, lost, reserve) {
 }
 
 // console.log(solution(5, [2, 4], [1, 3, 5]));
-console.log(solution(5, [2, 4], [3]));
+console.log(solution(5, [4, 2], [3, 5]));
