7d957a378678fbf9f227215ce7540e7e8abed4ff
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-06 15:58:14 +0900

    54차 3번 문제풀이

---
+++ live5/test54/문제3/김진용.py
@@ -0,0 +1,36 @@
+def solution(n, lost, reserve):
+    dic = {}
+
+    for student in range(1, n+1):
+        if student in reserve and student in lost:
+            dic[student] = 1
+        elif student in lost:
+            dic[student] = 0
+        elif student in reserve:
+            dic[student] = 2
+        else:
+            dic[student] = 1
+
+    for n in dic:
+        if n-1 in dic:
+            if dic[n-1] == 0 and dic[n] == 2:
+                dic[n] -= 1
+                dic[n-1] = 1
+        if n+1 in dic:
+            if dic[n+1] == 0 and dic[n] == 2:
+                dic[n] -= 1
+                dic[n+1] = 1
+
+    result = 0
+    for i in dic.values():
+        if i >= 1:
+            result += 1
+
+    return result
+
+
+
+print(solution(5, [2, 4], [1, 3, 5]))
+print(solution(5, [2, 3, 4], [1, 3, 5]))
+print(solution(5, [2, 4], [3]))
+print(solution(3, [3], [1]))
