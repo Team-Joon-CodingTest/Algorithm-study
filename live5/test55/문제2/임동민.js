549cb9eef6cf87e3974cd0906430e5bb647fa749
dongmin <dongmin11566@gmail.com>
2024-12-09 23:10:43 +0900

    55차 2번 문제풀이 (못품)

---
+++ live5/test55/문제2/임동민.js
@@ -0,0 +1,26 @@
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+function solution() {
+    const log = input.slice(1);
+    const map = Object.create(null);
+    const result = [];
+
+    log.forEach(e => {
+       const [name, status] = e.split(' ');
+       map[name] = status;
+    })
+
+    Object.entries(map).forEach(([name, status]) => {
+        if(status === 'enter') {
+            result.push(name);
+        }
+    })
+    result.sort((a,b) => b.localeCompare(a));
+    console.log(result.join('\n'));
+}
+
+solution();
