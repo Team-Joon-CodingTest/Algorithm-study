fc4003536fa3c9d7056468e12e9cbb9d6a1a83b9
gmlrude <qkrgmlrud00@gmail.com>
2024-12-09 23:41:06 +0900

    55차 3번 문제 다시 제출

--- live5/test55/문제3/박희경.py
+++ live5/test55/문제3/박희경.py
@@ -7,10 +7,12 @@ def solution(k, tangerine):
     for t in tangerine:
         type[t] += 1
     sorted_type = sorted(type.items(), key=lambda x: x[1], reverse=True)
-    cnt = 0
-    while cnt <= k:
-        for i in range(sorted_type):
-            cnt += sorted_type[i][1]
-        answer += i
+
+    for i in range(len(sorted_type)):
+        if k > 0:
+            k -= sorted_type[i][1]
+            answer += 1
+        elif k <= 0:
+            break
 
     return answer
