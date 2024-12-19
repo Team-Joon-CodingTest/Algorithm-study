8363b193420d576ced231830e8c3c7e28b852c39
gmlrude <qkrgmlrud00@gmail.com>
2024-12-20 04:04:29 +0900

    59차 2번 문제 다시 풀이

--- live5/test59/문제2/박희경.py
+++ live5/test59/문제2/박희경.py
@@ -7,34 +7,25 @@ grid = []
 for _ in range(n):
     grid.append(list(map(int, input().split())))
 
-prefix_grid_sum = [[0] * m for _ in range(n)]
-prefix_grid_sum[0][0] = grid[0][0]
+prefix_sum = [[0] * (m + 1) for _ in range(n + 1)]  # [놓친 부분] 0으로 비워두고 자리 수 맞추기
 
-for i in range(n):
-    for j in range(m):
-        if i == 0:
-            prefix_grid_sum[i][j] = prefix_grid_sum[i][j-1] + grid[i][j]
-        elif j == 0:
-            prefix_grid_sum[i][j] = prefix_grid_sum[i-1][j] + grid[i][j]
-        else:
-            prefix_grid_sum[i][j] = grid[i][j] + prefix_grid_sum[i-1][j] + prefix_grid_sum[i][j-1] - prefix_grid_sum[i-1][j-1]
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        prefix_sum[i][j] = (
+            grid[i - 1][j - 1]
+            + prefix_sum[i - 1][j]
+            + prefix_sum[i][j - 1]
+            - prefix_sum[i - 1][j - 1]
+        )
 
 k = int(input())
 for _ in range(k):
     i, j, x, y = map(int, input().split())
 
-    i, j, x, y = i - 1, j - 1, x - 1, y - 1
-
-    # 여기부터 다시
-
-
-
-
-"""
-2 3
-1 2 4
-8 16 32
-2
-1 2 1 2
-1 3 2 3
-"""
+    result = (
+            prefix_sum[x][y]
+            - prefix_sum[i - 1][y]
+            - prefix_sum[x][j - 1]
+            + prefix_sum[i - 1][j - 1]
+    )
+    print(result)
