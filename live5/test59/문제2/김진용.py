576a442281eca5f5f298da244af5b05cc0dbc626
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-20 08:22:44 +0900

    59차 3번 문제풀이

--- live5/test59/문제2/김진용.py
+++ live5/test59/문제2/김진용.py
@@ -1,31 +1,18 @@
 import sys
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
-ary = []
-for _ in range(n):
-    ary.append(list(map(int, input().split())))
-print(ary)
 
-simple_sum = [0]
+ary = [list(map(int, input().split())) for i in range(n)]
+# print(ary)
+dp = [[0]*(m+1) for _ in range(n+1)]
+# print(dp)
 
-for i in range(n):
-    for j in range(m):
-        simple_sum.append(simple_sum[-1] + ary[i][j])
-print(simple_sum)
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = ary[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+# print(dp)
 
-sum_ary = [[0] for i in range(n)]
-print(sum_ary)
-
-c = 0
-for i in range(n):
-    for j in range(m):
-        c += 1
-        sum_ary[i].append(simple_sum[c])
-print(sum_ary)
-
-m = int(input())
-for _ in range(m):
+c = int(input())
+for _ in range(c):
     i, j, x, y = map(int, input().split())
-    if i != x or j != y:
-        print(sum_ary[x][y] - sum_ary[)
+    print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
