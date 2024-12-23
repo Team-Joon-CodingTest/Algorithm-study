0905946fc2f283c2e15f1c4610350e0f0c3bac7a
gmlrude <qkrgmlrud00@gmail.com>
2024-12-23 23:26:47 +0900

    61차 3번 문제 다시 풀이

--- live6/test61/문제3/박희경.py
+++ live6/test61/문제3/박희경.py
@@ -1,19 +1,8 @@
-"""
-푸는중
-"""
-
 def solution(citations):
     answer = 0
-    citations = sorted(citations)  # [0, 1, 3, 5, 6]
-    n = len(citations)
+    citations = sorted(citations, reverse=True)
 
-    start, end = 0, n
-    mid = (start + end) // 2
-
-    while True:
-        # h_index = 0
-        if mid <= len(citations[mid:end + 1]):
-            return citations[mid]
-        else:
+    for idx, c in enumerate(citations, start=1):
+        answer = max(answer, min(idx, c))
 
     return answer
