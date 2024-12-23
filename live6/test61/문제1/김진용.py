c6ae2783a8c02dbd2a8ac6890a39cb981f26c3fa
kingjinyong <wlsdydwhswkf@gmail.com>
2024-12-23 22:57:37 +0900

    61차 1번 문제풀이

---
+++ live6/test61/문제1/김진용.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+t = int(input())
+
+
+def travel(contry, cnt):
+    visited[contry] = 1
+
+    for i in ary[contry]:
+        if visited[i] == 0:
+            cnt = travel(i, cnt + 1)
+    return cnt
+
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    ary = [[] for _ in range(1, n + 1 + 1)]
+    visited = [0] * (n + 1 + 1)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        ary[a].append(b)
+        ary[b].append(a)
+    print(travel(1, 0))
