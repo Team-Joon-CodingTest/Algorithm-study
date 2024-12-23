a74a560b7b8c1fd8d9b1e4d46022c666f7466d12
Eric <hgh1057@gmail.com>
2024-12-24 00:21:55 +0900

    61차 3번 문제풀이

--- live6/test61/문제3/황장현.js
+++ live6/test61/문제3/황장현.js
@@ -1,15 +1,15 @@
 function solution(citations) {
-  const sum = citations.reduce((prefix, curr) => prefix + curr, 0);
-  while (1) {
-    const H_Index = sum / citations.length;
-    let plus = 0;
-    let minus = 0;
-    for (let i = 0; i < citations.length; i++) {
-      if (citations[i] >= H_Index) plus++;
-      else minus++;
-    }
-    if (plus === H_Index) return plus;
+  citations.sort((a, b) => b - a);
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] <= i) return i;
   }
+  return citations.length;
 }
 
-console.log(solution([3, 0, 6, 1, 5]));
+console.log(solution([3, 3, 3, 4]));
+// console.log(solution([10, 9, 8, 4, 3, 1]));
+// console.log(solution([4, 0, 6, 1, 5]));
+// console.log(solution([1, 2, 3, 4, 5]));
+// console.log(solution([0, 0, 0, 0]));
+// console.log(solution([10, 8, 5, 4, 3]));
+// console.log(solution([5, 6, 7, 8, 9]));
