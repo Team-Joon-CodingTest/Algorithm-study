210d36c9a0f3b47c12625c66849e101d227f147d
Eric <hgh1057@gmail.com>
2024-12-26 21:41:19 +0900

    62차 2번 문제풀이

--- live6/test62/문제2/황장현.js
+++ live6/test62/문제2/황장현.js
@@ -10,15 +10,22 @@ function solution(input) {
   const levels = input[1];
   levels.unshift(0);
   const Q = input[2][0];
+  const result = [];
 
-  const prefixSum = Array(N + 1).fill(0);
-  for (let i = 0; i < N; i++) {}
-
-  console.log(prefixSum);
+  const prefixSum = Array(N).fill(0);
+  for (let i = 1; i <= N; i++) {
+    prefixSum[i] = prefixSum[i - 1];
+    if (levels[i] > levels[i + 1]) {
+      prefixSum[i]++;
+    }
+  }
 
   for (let i = 3; i < 3 + Q; i++) {
     const [x, y] = input[i];
+    const miss = prefixSum[y - 1] - prefixSum[x - 1];
+    result.push(miss);
   }
+  return result.join('\n');
 }
 
 console.log(solution(input));
