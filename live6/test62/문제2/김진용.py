b0a6abc463ee0df0b07d4f7ad5f673577978b684
kingjinyong <163799220+kingjinyong@users.noreply.github.com>
2024-12-27 14:21:54 +0900

    [김진용] 62차 라이브 코테 제출 (#511)
    * 61차 1번 문제풀이
    
    * 61차 2번 문제풀이
    
    * 61차 3번 문제풀이
    
    * 61차 문제 3번 다시 풀기
    
    * 62차 1번 문제풀이
    
    * 62차 2번 문제풀이
    
    * 문제 2 수정

---
+++ live6/test62/문제2/김진용.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+akvo = list(map(int, input().split()))
+
+mistake_s = 0
+dp = [0]*(n+1)
+for i in range(n):
+    if akvo[i] < akvo[i-1]:
+        mistake_s += 1
+
+    dp[i] = mistake_s
+
+q = int(input())
+
+for _ in range(q):
+    x, y = map(int, input().split())
+
+    print(dp[y-1] - dp[x-1])
