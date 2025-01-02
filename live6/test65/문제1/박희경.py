f305748c04105c6de307c0eef3d887a81dc679d6
gmlrude <qkrgmlrud00@gmail.com>
2025-01-03 02:41:58 +0900

    65차 1번 문제 풀이 (참고)

--- live6/test65/문제1/박희경.py
+++ live6/test65/문제1/박희경.py
@@ -8,17 +8,41 @@ board = []
 for _ in range(n):
     board.append(list(map(str, input().rstrip())))
 
-sum = 0
-current = board[0][0]
 
+def counting():
+    max_cnt = 1
+    for i in range(n):
+        cnt_row = cnt_col = 1
+        for j in range(n - 1):
+            # 가로
+            if board[i][j] == board[i][j + 1]:  # 같은 색이면
+                cnt_row += 1
+            else:
+                cnt_row = 1  # 다른 색이면 카운트 다시 시작
+            max_cnt = max(max_cnt, cnt_row)
 
-def max_candy(i, j, cnt):
-    global n
-    current_color = board[i][j]
-    dx = [0, 1]
-    dy = [1, 0]
+        for j in range(n - 1):
+            # 세로
+            if board[j][i] == board[j + 1][i]:
+                cnt_col += 1
+            else:
+                cnt_col = 1
+            max_cnt = max(max_cnt, cnt_col)
+    return max_cnt
 
-    if 0 <= i < n and 0 <= j < n:
+res = 1
+for i in range(n):
+    for j in range(n - 1):
+        if n > j + 1 and board[i][j] != board[i][j + 1]:
+            # 아래와 교환
+            board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
+            res = max(res, counting())
+            # 복구
+            board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
+        if n > i + 1 and board[i][j] != board[i + 1][j]:
+            # 왼쪽과 교환
+            board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
+            res = max(res, counting())
+            board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
 
-
-max_candy(0, 0, 0)
+print(res)
