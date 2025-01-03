926e7cb0755e2398710cf3c57cd4c51d052d28f8
gmlrude <qkrgmlrud00@gmail.com>
2025-01-03 16:58:57 +0900

    66차 2번 문제 다시 풀이

--- live6/test66/문제2/박희경.py
+++ live6/test66/문제2/박희경.py
@@ -4,18 +4,21 @@ input = sys.stdin.readline
 
 n = int(input())
 map = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
 
 
 def jump(x, y):
     dx = [0, map[x][y]]
     dy = [map[x][y], 0]
+    visited[x][y] = 1
     for i in range(2):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx < n and ny < n:
+        if nx < n and ny < n and visited[nx][ny] == 0:
             if map[nx][ny] == -1:
                 return True
-            jump(nx, ny)
+            if jump(nx, ny):
+                return True
     return False
 
 
