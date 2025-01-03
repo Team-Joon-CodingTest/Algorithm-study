3e495c359d6876ccf2c389a306dd79f2039d9b59
Eric <hgh1057@gmail.com>
2025-01-03 13:52:51 +0900

    65차 1번 문제풀이

--- live6/test65/문제1/황장현.js
+++ live6/test65/문제1/황장현.js
@@ -2,55 +2,62 @@ const input = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
   .toString()
   .trim()
-  .split('\n')
-  .map((el) => el.split(' '));
+  .split('\n');
 
 function solution(input) {
-  const N = Number(input[0][0]);
-  const board = input.slice(1).map((subArray) => subArray[0].split(''));
-  const isVisited = Array.from({ length: N }, () => Array(N).fill(0));
-  console.log(board);
+  const N = Number(input[0]);
+  const board = input.slice(1).map((row) => row.split(''));
+  let max = 0;
 
-  let max = Infinity;
-  let tempMax = 1;
-  function dfs(x, y, prevMove, step) {
-    const directions = [
-      [-1, 0, 'Left'],
-      [1, 0, 'Down'],
-      [0, -1, 'Up'],
-      [0, 1, 'Right'],
-    ];
-    let currentStep = step;
+  function getMaxCandy(board) {
+    let maxLength = 0;
 
-    for (const [dx, dy, direction] of directions) {
-      const nextX = x + dx;
-      const nextY = y + dy;
-
-      if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N) {
-        const currentColor = board[x][y];
-        const nextColor = board[nextX][nextY];
-        console.log(x, y, direction);
-
-        if (
-          !isVisited[nextX][nextY] &&
-          currentColor === nextColor &&
-          (step > 1 ? prevMove === direction : true)
-        ) {
-          currentStep++;
-          dfs(nextX, nextY, direction, currentStep);
-        } else return;
+    for (let i = 0; i < N; i++) {
+      let count = 1;
+      for (let j = 1; j < N; j++) {
+        if (board[i][j] === board[i][j - 1]) {
+          count++;
+        } else {
+          maxLength = Math.max(maxLength, count);
+          count = 1;
+        }
       }
+      maxLength = Math.max(maxLength, count);
     }
+
+    for (let j = 0; j < N; j++) {
+      let count = 1;
+      for (let i = 1; i < N; i++) {
+        if (board[i][j] === board[i - 1][j]) {
+          count++;
+        } else {
+          maxLength = Math.max(maxLength, count);
+          count = 1;
+        }
+      }
+      maxLength = Math.max(maxLength, count);
+    }
+
+    return maxLength;
   }
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      isVisited[i][j] = 1;
-      dfs(i, j, '', 0);
+      if (j + 1 < N) {
+        [board[i][j], board[i][j + 1]] = [board[i][j + 1], board[i][j]];
+        max = Math.max(max, getMaxCandy(board));
+        [board[i][j], board[i][j + 1]] = [board[i][j + 1], board[i][j]];
+      }
+
+      if (i + 1 < N) {
+        [board[i][j], board[i + 1][j]] = [board[i + 1][j], board[i][j]];
+        max = Math.max(max, getMaxCandy(board));
+        [board[i][j], board[i + 1][j]] = [board[i + 1][j], board[i][j]];
+      }
     }
   }
 
-  //   if (max === N) return N;
+  return max;
 }
 
 console.log(solution(input));
