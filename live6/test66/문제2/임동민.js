6e8d85bf8531421c3234948538e7bc86e3367133
dongmin <dongmin11566@gmail.com>
2025-01-03 17:28:19 +0900

    66차 2번 문제풀이

--- live6/test66/문제2/임동민.js
+++ live6/test66/문제2/임동민.js
@@ -12,23 +12,30 @@ function solution() {
         [1,0],
         [0,1]
     ];
+    let result = false;
+    const visited = Array.from({length: N}, ()=> new Array(N).fill(false));
 
     function recursive(x,y){
-        if(x<0 || y<0 || x>=N || y>=N) return;
+        if(x<0 || y<0 || x>=N || y>=N || visited[x][y]) return;
         if(board[x][y] === -1){
-            return console.log('HaruHaru');
+            result = true;
+            return;
         }
 
         const jump = board[x][y];
+        visited[x][y] = true;
 
         for(const [dx,dy] of directions){
-
+            const nx = x + jump * dx;
+            const ny = y + jump * dy;
+            recursive(nx,ny);
         }
-
 
     }
 
     recursive(0,0);
+
+    console.log(result ? "HaruHaru" : "Hing");
 
 
 }
