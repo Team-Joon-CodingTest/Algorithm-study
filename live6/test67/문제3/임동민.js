dfc825cf174e58e2908f3a63310c35dbfd21fb9b
dongmin <dongmin11566@gmail.com>
2025-01-06 23:54:45 +0900

    67차 3번 문제풀이

--- live6/test67/문제3/임동민.js
+++ live6/test67/문제3/임동민.js
@@ -1,29 +1,32 @@
 function solution(str1, str2) {
-    str1 = str1.replace(/[^a-zA-Z0-9]/g, '');
     str1 = str1.toLowerCase();
-
-    str2 = str2.replace(/[^a-zA-Z0-9]/g, '');
     str2 = str2.toLowerCase();
 
     const stack1 = [];
     const stack2 = [];
 
-    for(let i=0; i<str1.length-1; i++){
-        stack1.push(str1[i] + str1[i+1]);
+    for (let i = 0; i < str1.length - 1; i++) {
+        const pair = str1[i] + str1[i + 1];
+        if (/^[a-z]{2}$/.test(pair)) {
+            stack1.push(pair);
+        }
     }
 
-    for(let i=0; i<str2.length-1; i++){
-        stack2.push(str2[i] + str2[i+1]);
+    for (let i = 0; i < str2.length - 1; i++) {
+        const pair = str2[i] + str2[i + 1];
+        if (/^[a-z]{2}$/.test(pair)) {
+            stack2.push(pair);
+        }
     }
 
     const gyo = [];
-    const temp = [...stack2];
+    const tempStack2 = [...stack2];
 
-    for(const e of stack1){
-        const idx = temp.indexOf(e);
-        if(idx !== -1){
+    for (const e of stack1) {
+        const idx = tempStack2.indexOf(e);
+        if (idx !== -1) {
             gyo.push(e);
-            temp.splice(idx,1);
+            tempStack2.splice(idx, 1);
         }
     }
 
@@ -41,6 +44,5 @@ function solution(str1, str2) {
         return 65536;
     }
 
-    return (gyo.length / hap.length) * 65536;
-
+    return Math.floor((gyo.length / hap.length) * 65536);
 }
