dfc2bbb77edabb8401bb081ce9e3932a8d5ef04b
jihye9871 <jihye9871@naver.com>
2025-01-09 18:50:50 +0900

    67차 1번 문제풀이

--- live6/test67/문제1/최지혜.py
+++ live6/test67/문제1/최지혜.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 
+
 def mergeSort(L):
     if len(L) == 1:
         return L
@@ -9,6 +10,39 @@ def mergeSort(L):
     left = mergeSort(L[:mid])
     right = mergeSort(L[mid:])
     
+    L2 = []
+    i = 0
+    j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            L2.append(left[i])
+            ans.append(left[i])
+            i += 1
+        else:
+            L2.append(right[j])
+            ans.append(right[j])
+            j += 1
+    
+    while i < len(left):
+        L2.append(left[i])
+        ans.append(left[i])
+        i += 1
+        
+    while j < len(right):
+        L2.append(right[j])
+        ans.append(right[j])
+        j += 1
+        
+    return L2
 
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
+
+ans = []
+mergeSort(a)
+
+if len(ans) >= k:
+    print(ans[k-1])
+else:
+    print(-1) 
