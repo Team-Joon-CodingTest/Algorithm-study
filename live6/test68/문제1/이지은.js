9da5b19cd97d5341e2519380af92060c8bea53e7
egg-silver <v6984@naver.com>
2025-01-09 21:28:23 +0900

    68차 1번 (푸는 중)

---
+++ live6/test68/문제1/이지은.js
@@ -0,0 +1,23 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+function solution(input) {
+  const [e, s, m] = input;
+
+  let year = e;
+
+  while (year <= 7980) {
+    if (year % 28 === s && year % 19 === m) {
+      break;
+    } else {
+      year += 15;
+    }
+  }
+  return year
+}
+
+console.log(solution(input));
