5b0d4238ad0ec958735faa886475cdfc6f281016
gmlrude <qkrgmlrud00@gmail.com>
2025-01-10 16:26:35 +0900

    69차 3번 문제 다시 풀이

--- live6/test69/문제3/박희경.py
+++ live6/test69/문제3/박희경.py
@@ -13,11 +13,9 @@ def solution(want, number, discount):
         for i in range(a, a + 10):
             if discount[i] in want:
                 tmp_product[discount[i]] -= 1
-        print(tmp_product)
+
         # 모두 0일 때 카운팅
-        # for value in tmp_product.values():
-        #     if value > 0:
-        #         break
-        # answer += 1
+        if all(value <= 0 for value in tmp_product.values()):
+            answer += 1
 
     return answer
