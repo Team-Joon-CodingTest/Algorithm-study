c37fd587bdc5a16b7088c34b0df3edc8be0fd623
dongmin <dongmin11566@gmail.com>
2025-01-16 16:48:20 +0900

    70차 3번 문제풀이

--- live6/test70/문제3/임동민.js
+++ live6/test70/문제3/임동민.js
@@ -1,17 +1,17 @@
 function solution(priorities, location) {
-    const target = priorities[location];
-    const queue = [...priorities];
+    let queue = priorities.map((priority, idx) => [priority, idx]);
     let result = 0;
 
-    while(queue[0] !== target){
-        if(queue[0] >= Math.max(...queue)){
-            queue.shift();
-            result++;
+    while (true) {
+        const [currPriority, currIndex] = queue.shift();
+
+        if (queue.some(([p]) => p > currPriority)) {
+            queue.push([currPriority, currIndex]);
         } else {
-            queue.push(queue.shift());
+            result++;
+            if (currIndex === location) {
+                return result;
+            }
         }
     }
-
-    return result;
-
 }
