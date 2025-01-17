214e7ac497137e105cbef8a2621a1c1636c309b0
gmlrude <qkrgmlrud00@gmail.com>
2025-01-17 13:49:35 +0900

    71차 3번 문제 다시 풀이

--- live7/test71/문제3/박희경.py
+++ live7/test71/문제3/박희경.py
@@ -1,21 +1,24 @@
 def solution(topping):
     answer = 0
 
-    left = {}
-    right = {}
+    left = set()
+    right = {}  # 놓친 부분: 토핑을 딕셔너리로 카운트 하는 것
+    # 토핑 개수 카운트
     for t in topping:
         if t in right:
             right[t] += 1
         else:
             right[t] = 1
-    print(right)
 
+    # 토핑 나누기
     for t in topping:
-        left[t] = 1
+        left.add(t)
         right[t] -= 1
 
-        if len(left.values()) == sum(right.values()):
-            print(t, "일때,", left, right)
+        if right[t] == 0:
+            del (right[t])
+
+        if len(left) == len(right.keys()):
             answer += 1
 
     return answer
