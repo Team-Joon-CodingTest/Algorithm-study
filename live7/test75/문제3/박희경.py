af08d198dbc5f6bfa4c51e65d0288aa032de2826
gmlrude <qkrgmlrud00@gmail.com>
2025-01-31 16:53:21 +0900

    75차 3번 문제 다시 풀이

--- live7/test75/문제3/박희경.py
+++ live7/test75/문제3/박희경.py
@@ -1,13 +1,10 @@
 from itertools import *
 
-
 def solution(word):
-    answer = 0
-
     alpha = ['A', 'E', 'I', 'O', 'U']
     dict = []
     for i in range(1, 6):
         for p in product(alpha, repeat=i):  # 중복순열
             dict.append(''.join(p))
     dict = sorted(dict)
-    return answer
+    return dict.index(word) + 1
